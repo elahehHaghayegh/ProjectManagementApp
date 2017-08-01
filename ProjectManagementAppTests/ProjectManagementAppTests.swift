5a18fc528b9f45f59ee0ae9177a792e2e851c058
elaheh <elaheh@elahehs-macbook.local>
2017-08-01 13:34:38 -0400

    Initial Commit

---
+++ ProjectManagementAppTests/ProjectManagementAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProjectManagementAppTests.swift
+//  ProjectManagementAppTests
+//
+//  Created by elaheh on 2017-08-01.
+//  Copyright © 2017 elaheh. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectManagementApp
+
+class ProjectManagementAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
